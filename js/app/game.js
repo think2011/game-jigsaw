2dda1c02dd6898e98aa77b2bc94844d1ae62c1f9
think2011 <452125301@qq.com>
2016-09-27 18:02:11 +0800

    feat requirejs

--- js/app/game.js
+++ js/app/game.js
@@ -10,19 +10,31 @@ define(['tools'], function (tools) {
             this.render('./img/pic.gif')
         },
 
-        render: function (picUrl) {
+        render: function (picUrl, sizeX, sizeY) {
+            if (!sizeX) sizeX = 3
+            if (!sizeY) sizeY = 3
+
             var img    = new Image()
             img.onload = function () {
-                var html = '<div class="img" style="\nwidth: !{width}!{unit};\nheight: !{height}!{unit};\nbackground: url(" !{url}") no-repeat !{x}!{unit} !{y}!{unit};\n"></div>'.render({
-                    width : 100,
-                    height: 100,
-                    unit  : 'xx',
+                var width  = img.naturalWidth
+                var height = img.naturalHeight
+                var total  = sizeX * sizeY
+
+                var html = '<div class="img" style="\nwidth: !{width}!{unit};\nheight: !{height}!{unit};\nbackground: url("!{url}") no-repeat !{x}!{unit} !{y}!{unit};\n"></div>'.render({
+                    width : width / sizeX,
+                    height: height / sizeY,
+                    unit  : 'px',
+                    url   : picUrl,
                     x     : 1,
                     y     : 2
                 })
 
-                console.log(img.naturalWidth)
-                console.log(img.naturalHeight)
+                var i = 0;
+                for (i; i < total; i++) {
+                    console.log(i)
+                }
+
+                console.log(html)
             }
 
             img.src = picUrl
