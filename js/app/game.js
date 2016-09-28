26cc9963ebc8fbf4da734facd6ae010c5475771a
think2011 <452125301@qq.com>
2016-09-28 15:31:47 +0800

    temp

--- js/app/game.js
+++ js/app/game.js
@@ -1,5 +1,9 @@
 define(['tools'], function (tools) {
     var Class = function () {
+        this.sizeX   = 3
+        this.sizeY   = 3
+        this.axisBox = []
+
         this.init()
     }
 
@@ -7,34 +11,52 @@ define(['tools'], function (tools) {
         construct: Class,
 
         init: function () {
-            this.render('./img/pic.gif')
+            this.render('./img/pic.gif', this.sizeX, this.sizeY)
         },
 
         render: function (picUrl, sizeX, sizeY) {
+            var that = this
+
             if (!sizeX) sizeX = 3
             if (!sizeY) sizeY = 3
 
             var img    = new Image()
             img.onload = function () {
-                var width  = img.naturalWidth
-                var height = img.naturalHeight
-                var total  = sizeX * sizeY
+                var width      = img.naturalWidth
+                var height     = img.naturalHeight
+                var unit       = 'px'
+                var $container = $('<div class="jigsaw-container"></div>')
 
-                var html = '<div class="img" style="\nwidth: !{width}!{unit};\nheight: !{height}!{unit};\nbackground: url("!{url}") no-repeat !{x}!{unit} !{y}!{unit};\n"></div>'.render({
-                    width : width / sizeX,
-                    height: height / sizeY,
-                    unit  : 'px',
-                    url   : picUrl,
-                    x     : 1,
-                    y     : 2
-                })
+                $container.css({width: width, height: height})
 
-                var i = 0;
-                for (i; i < total; i++) {
-                    console.log(i)
+                for (var i = 0; i < sizeX; i++) {
+                    var params = {
+                        width : width / sizeX,
+                        height: height / sizeY,
+                        unit  : 'px',
+                        url   : picUrl
+                    }
+
+                    for (var j = 0; j < sizeY; j++) {
+                        params.x = params.width * j
+                        params.y = params.height * i
+
+                        var $html = $('<div class="img"></div>').css({
+                            width              : params.width + unit,
+                            height             : params.height + unit,
+                            background         : 'url("' + params.url + '") no-repeat',
+                            left               : params.x,
+                            top                : params.y,
+                            backgroundPositionX: -params.x + unit,
+                            backgroundPositionY: -params.y + unit
+                        })
+
+                        that.axisBox.push([params.x, params.y])
+                        $container.append($html)
+                    }
                 }
 
-                console.log(html)
+                $('body').append($container)
             }
 
             img.src = picUrl
