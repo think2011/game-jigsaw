3a0564e60eaebe06b7539fd1ce7e8aa5f2c88dbd
think2011 <452125301@qq.com>
2016-10-14 19:47:00 +0800

    feat s2

---
+++ js/lib/lottery-layer/lottery-layer.js
@@ -0,0 +1,13 @@
+;(function () {
+    var swiper = new Swiper('.lottery-layer', {
+        pagination         : '.swiper-pagination',
+        width              : hotcss.rem2px(21.034666666666666, document.documentElement.offsetWidth),
+        effect             : 'coverflow',
+        centeredSlides     : true,
+        slideToClickedSlide: true,
+        paginationClickable: true,
+        slidesPerView      : 2,
+    })
+
+
+})()
